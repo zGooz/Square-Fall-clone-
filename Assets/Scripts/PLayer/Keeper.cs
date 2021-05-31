948c53dc7694a0844736658c7d601a9daf5120f9
zGooz <ya.asde-12a@yandex.ru>
2021-05-31 11:07:12 +0300

    The completed menu (there is no sounds in the game, the 'On / Off sound button' does not perform your functions). The menu now activates and deactivates objects on the scene and other menu items.

--- Assets/Scripts/PLayer/Keeper.cs
+++ Assets/Scripts/PLayer/Keeper.cs
@@ -1,14 +1,22 @@
 
 using UnityEngine;
+using UnityEngine.Events;
 
 
 [RequireComponent(typeof(CircleCollider2D))]
 
 public class Keeper : MonoBehaviour
 {
+    public event UnityAction Keep;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var other = collision.gameObject;
+
+        if (other.TryGetComponent(out Bonus _))
+        {
+            Keep?.Invoke();
+        }
 
         if (other.TryGetComponent(out Square _))
         {
